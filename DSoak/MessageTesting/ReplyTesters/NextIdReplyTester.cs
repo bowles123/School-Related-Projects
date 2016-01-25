6e319889f086efe89ba1052a92ccf43942aac055
Stephen Clyde <swc@mdsc.com>
2016-01-25 11:56:41 -0700

    Added some more test cases

---
+++ DSoak/MessageTesting/ReplyTesters/NextIdReplyTester.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Messages;
+using Messages.ReplyMessages;
+using SharedObjects;
+
+namespace MessageTesting.ReplyTesters
+{
+    [TestClass]
+    public class NextIdTester
+    {
+        [TestMethod]
+        public void NextIdReply_TestEverything()
+        {
+            NextIdReply r1 = new NextIdReply();
+            Assert.IsFalse(r1.Success);
+            Assert.IsNull(r1.Note);
+
+            NextIdReply r2 = new NextIdReply()
+            {
+                Success = true,
+                Note = "Testing"
+            };
+
+            Assert.IsTrue(r2.Success);
+            Assert.AreEqual("Testing", r2.Note);
+            
+            byte[] bytes = r2.Encode();
+
+            Message m2 = Message.Decode(bytes);
+            NextIdReply r3 = m2 as NextIdReply;
+            Assert.IsNotNull(r3);
+            Assert.AreNotSame(r2, r3);
+            Assert.IsTrue(r3.Success);
+            Assert.AreEqual(r2.Note, r3.Note);
+        }
+    }
+}
--- DSoak/MessageTesting/RequestTesters/JoinGameRequestTester.cs
+++ DSoak/MessageTesting/RequestTesters/JoinGameRequestTester.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Messages;
 using Messages.RequestMessages;
+using SharedObjects;
 
 namespace MessageTesting
 {
@@ -11,9 +12,67 @@ namespace MessageTesting
     public class JoinGameRequestTester
     {
         [TestMethod]
-        public void JoinGameRequest_TestEverything()
+        public void JoinGameRequest_TestDefaultConstructor()
         {
+            JoinGameRequest request = new JoinGameRequest();
+            Assert.AreEqual(0, request.GameId);
+            Assert.IsNull(request.Player);
+        }
 
+        [TestMethod]
+        public void JoinGameRequest_TestProperties()
+        {
+            ProcessInfo player = new ProcessInfo()
+            {
+                ProcessId = 1,
+                Type = ProcessInfo.ProcessType.Player,
+                EndPoint = new PublicEndPoint() { HostAndPort = "abc.usu.edu:12345" },
+                Label = "Test Process",
+                Status = ProcessInfo.StatusCode.Initializing,
+                AliveTimestamp = DateTime.Now 
+            };
+
+            JoinGameRequest request = new JoinGameRequest()
+            {
+                GameId = 10,
+                Player = player
+            };
+            Assert.AreEqual(10, request.GameId );
+            Assert.AreSame(player,request.Player);
+        }
+
+        [TestMethod]
+        public void JoinGameRequest_EncodeAndDecode()
+        {
+            ProcessInfo player = new ProcessInfo()
+            {
+                ProcessId = 1,
+                Type = ProcessInfo.ProcessType.Player,
+                EndPoint = new PublicEndPoint() { HostAndPort = "abc.usu.edu:12345" },
+                Label = "Test Process",
+                Status = ProcessInfo.StatusCode.Initializing,
+                AliveTimestamp = DateTime.Now
+            };
+
+            JoinGameRequest request = new JoinGameRequest()
+            {
+                GameId = 10,
+                Player = player
+            };
+
+            byte[] bytes = request.Encode();
+
+            Message decodedMessage = Message.Decode(bytes);
+
+            JoinGameRequest decodedRequest = decodedMessage as JoinGameRequest;
+            Assert.IsNotNull(decodedRequest);
+            Assert.AreEqual(request.GameId, decodedRequest.GameId);
+            Assert.IsNotNull(decodedRequest.Player);
+            Assert.AreEqual(request.Player.ProcessId, decodedRequest.Player.ProcessId);
+            Assert.AreEqual(request.Player.Type, decodedRequest.Player.Type);
+            Assert.AreEqual(request.Player.EndPoint, decodedRequest.Player.EndPoint);
+            Assert.AreEqual(request.Player.Status, decodedRequest.Player.Status);
+            Assert.IsNull(decodedRequest.Player.AliveTimestamp);
         }
     }
 }
