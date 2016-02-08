ceef59dd0a1a033c90caf388314d10cd6fc8780c
Stephen Clyde <swc@mdsc.com>
2016-02-08 13:25:52 -0700

    Added Tcp Example

---
+++ Examples/TcpExample/TcpInitiator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TcpInitiator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string messageTosend = (args.Length > 0) ? args[0] : "Nothing to says";
+            TcpClient client = new TcpClient("127.0.01", 15000);
+            while (!client.Connected)
+                Thread.Sleep((100));
+            NetworkStream stream = client.GetStream();
+
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(messageTosend);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
---
+++ Examples/TcpExample/TcpResponder/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpResponder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint myEp = new IPEndPoint(IPAddress.Any, 15000);
+            TcpListener server = new TcpListener(myEp);
+            server.Start();
+
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[256];
+                bool stayConnected = true;
+                while (stayConnected)
+                {
+                    try
+                    {
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        if (bytesRead > 0)
+                        {
+                            string data = System.Text.Encoding.ASCII.GetString(buffer);
+                            Console.WriteLine("Received {0} bytes: {1}", bytesRead, data);
+                            if (data == "$" || data == "*")
+                                stayConnected = false;
+
+                            if (data == "$")
+                                keepGoing = false;
+                        }
+                    }
+                    catch
+                    {
+                        stayConnected = false;
+                    }
+                }
+            }
+
+        }
+    }
+}
