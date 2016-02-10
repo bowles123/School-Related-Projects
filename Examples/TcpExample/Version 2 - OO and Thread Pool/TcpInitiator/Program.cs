369301f6283795ab20d9f581eb55a27d3875dae0
Stephen Clyde <swc@mdsc.com>
2016-02-10 15:52:00 -0700

    Created new version on TcpExample and move old version in another directory.

---
+++ Examples/TcpExample/Version 2 - OO and Thread Pool/TcpInitiator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TcpInitiator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = (args.Length > 0) ? args[0] : "Nothing to says";
+            int repeatCount = 1;
+            if (args.Length > 1)
+                Int32.TryParse(args[1], out repeatCount);
+
+            Sender sender = new Sender() { MessageToSend = message, RepeatCount = repeatCount};
+            sender.Run();
+
+        }
+    }
+}
