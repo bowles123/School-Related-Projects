95f1b00300210737069d3ca3c4bd303fdcac4daf
Stephen Clyde <swc@mdsc.com>
2016-02-23 10:49:26 -0700

    Added deadlock example

---
+++ Examples/DeadLockExamples/Version1-Deadlock/TwoThreads/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwoThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkerA workerA = new WorkerA();
+            workerA.Start();
+
+            WorkerB workerB = new WorkerB();
+            workerB.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Examples/DeadLockExamples/Version1-Deadlock/TwoThreads/ResourceCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoThreads
+{
+    public static class ResourceCollection
+    {
+        public static Resource A { get; set; }
+        public static Resource B { get; set; }
+
+        public static object LockA { get; set; }
+        public static object LockB { get; set; }
+
+        static ResourceCollection()
+        {
+            A = new Resource() { Name = "A", Data = "1234567890"};
+            B = new Resource() { Name = "B", Data = "9876543210"};
+            
+            LockA = new object();
+            LockB = new object();
+        }
+    }
+}
