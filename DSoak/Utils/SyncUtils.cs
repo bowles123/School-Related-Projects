e18859bff8cd08e234e216cfe89ea97dd03ec046
Stephen Clyde <swc@mdsc.com>
2016-03-30 21:31:39 -0600

    Added SyncUtils with a method for waiting for a condition

---
+++ DSoak/Utils/SyncUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Utils
+{
+    public static class SyncUtils
+    {
+        public static bool WaitForCondition(Func<bool> condition, int timeout = 1000, int step = 100)
+        {
+            bool result = false;
+
+            int remainingTime = timeout;
+            while (remainingTime > 0 && !(result=condition()))
+            {
+                Thread.Sleep(step);
+                remainingTime -= step;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DSoak/UtilsTesting/SyncUtilsTester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Utils;
+
+namespace UtilsTesting
+{
+    [TestClass]
+    public class SyncUtilsTester
+    {
+        private bool _flag;
+
+        [TestMethod]
+        public void SyncUtils_TestEverything()
+        {
+            // Case 0 - Timeout will occur
+            DateTime ts = DateTime.Now;
+            bool result = SyncUtils.WaitForCondition(() => _flag == true, 4000);
+            Assert.IsFalse(DateTime.Now.Subtract(ts).TotalMilliseconds < 4000);
+            Assert.IsFalse(result);
+
+
+            // Case 1 - Condition will be met before timeout
+            Timer timer = new Timer(SetFlag, null, 1000, 1000);
+
+            ts = DateTime.Now;
+            result = SyncUtils.WaitForCondition(() => _flag==true, 4000);
+            Assert.IsTrue(DateTime.Now.Subtract(ts).TotalMilliseconds<4000);
+            Assert.IsTrue(result);
+        }
+
+        private void SetFlag(object state)
+        {
+            _flag = true;
+        }
+
+    }
+}
