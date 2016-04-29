ca8320041338aee82ed08943a02e2667d10bfe4c
Stephen W. Clyde <swc@mdsc.com>
2016-04-29 16:49:19 -0400

    Added sample bat file with custom execution parameters

---
+++ DSoak/LaunchRegistryAndGameManager - Custom.bat
@@ -0,0 +1,3 @@
+start .\Deploy\Registry\Registry.exe --autostart
+timeout 2
+start .\Deploy\GameManager\GameManager.exe --bsexe="Y:\Dropbox\UnderGit\cs5200s16-instructor\DSoak-hw4\Deploy\BalloonStore\BalloonStore.exe"
